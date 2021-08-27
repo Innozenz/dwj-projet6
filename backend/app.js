84a44f45f21b526ff2067b94868e5470446cf841
Innozenz <mfitzjean@gmail.com>
2021-08-27 20:35:30 +0200

    Security

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require("express");
+const helmet = require("helmet");
 const bodyParser = require('body-parser');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user')
@@ -8,11 +9,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
+app.use(helmet());
 
 mongoose.connect(process.env.DATABASE_URL,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
