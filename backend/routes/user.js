09cf92deb845ebe042295cf28464fef1343211cc
Innozenz <mfitzjean@gmail.com>
2021-09-06 22:51:36 +0200

    Security

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const checkPassword = require("../middleware/check-password")
 const userCtrl = require('../controllers/user');
+const expressBouncer = require("express-bouncer")(5000, 600000, 3); // Contre les attaques de force Brute //
 
 router.post('/signup', checkPassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', expressBouncer.block, userCtrl.login);
 
 
 module.exports = router;
